9d46f2415e34761b6049f8d5928f5b59e4b93283
Brian Graham <graham.briank@gmail.com>
2015-10-14 14:08:54 -0700

    add ignore file, change to static rendering html

--- routes.py
+++ routes.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request
  
-app = Flask(__name__)      
+app = Flask(__name__, static_url_path='')      
  
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return app.send_static_file('index.html')
  
 if __name__ == '__main__':
   app.run()
